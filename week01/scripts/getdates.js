85acb1208f12913791c2a4d312904c42d271504c
nobrega <mmsud2014@gmail.com>
2024-06-18 18:45:19 -0300

    att

--- week01/scripts/getdates.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const yearSpan = document.getElementById('currentyear');
-    const lastModifiedParagraph = document.getElementById('lastModified');
-    
-    const currentYear = new Date().getFullYear();
-    yearSpan.textContent = currentYear;
-
-    lastModifiedParagraph.textContent = `Last Modification: ${document.lastModified}`;
-});
